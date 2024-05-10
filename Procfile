57ca94ad17641502b7131865e6d8617f8a7bcb98
LimaZurmati <urzalalima@gmail.com>
2024-05-10 18:58:34 +0000

    Deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn World_News.wsgi
--- World_News/settings.py
+++ World_News/settings.py
@@ -42,7 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary',
     'News',
 ]
 
@@ -129,6 +131,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES = 'loudinary_storage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
