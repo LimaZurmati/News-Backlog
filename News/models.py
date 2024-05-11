a51dae91c5b2f5d9b05f2b91288a91340d005cd5
LimaZurmati <urzalalima@gmail.com>
2024-05-11 20:01:54 +0000

    Create Data Modele class News

--- News/models.py
+++ News/models.py
@@ -2,5 +2,28 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
-
+STATUS = ((0, "Draft"), (1, "Published"))
 # Create your models here.
+class News(models.Model):
+    news_title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    news_author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="news_article"
+    )
+    news_image = CloudinaryField('image', default='placeholder')
+    news_excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    news_content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    released_status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(
+        User, related_name='news_like', blank=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return self.news_title
+
+    def number_of_likes(self):
+        return self.likes.count()
