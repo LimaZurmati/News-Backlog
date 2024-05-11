e6a670def17368cb2171a449a61607ca35f97ca0
LimaZurmati <urzalalima@gmail.com>
2024-05-11 20:19:04 +0000

    Create Comment Model

--- News/models.py
+++ News/models.py
@@ -27,3 +27,18 @@ class News(models.Model):
 
     def number_of_likes(self):
         return self.likes.count()
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="comments")
+    user_name = models.CharField(max_length=80)
+    user_email = models.EmailField()
+    Comment_content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.Comment_content} by {self.user_name}"
