6c70b827f78d750a0152fd6efaeb658846f7d8e3
LimaZurmati <urzalalima@gmail.com>
2024-05-12 18:46:23 +0000

    Create the view

--- News/views.py
+++ News/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from news.models import News
 
 # Create your views here.
+class NewsList(generic.ListView):
+    model = News
+    queryset = News.objects.filter(
+    released_status=1).order_by("-created_on")
+    template_name = "index.html"
+    paginate_by = 6
