1257766e30820b1f371deb95b29fd1ea124ac81b
LimaZurmati <urzalalima@gmail.com>
2024-05-12 17:41:26 +0000

    Building admin site

--- News/admin.py
+++ News/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
-from .models import News
+from .models import News, Comment      
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
-admin.site.register(News)
+@admin.register(News)
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ('news_content')
+    prepopulated_fields = {'slug': ('news_title',)}
+    list_filter = ('released_status', 'created_on')
+    search_fields = ['news_title', 'news_content']
+    list_display = ('news_title', 'slug', 'released_status', 'created_on')
+        
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user_name', 'Comment_content', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('user_name', 'Comment_content')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
+
+
+
+
+
