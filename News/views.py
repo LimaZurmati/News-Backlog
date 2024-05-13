34fe88212f2d26dfc9be21254ab33a17c1dee24c
LimaZurmati <urzalalima@gmail.com>
2024-05-13 14:05:30 +0000

    Create a template to render the view

--- News/views.py
+++ News/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.views import generic
-from news.models import News
+from .models import News
 
 # Create your views here.
 class NewsList(generic.ListView):
     model = News
     queryset = News.objects.filter(
     released_status=1).order_by("-created_on")
+    print('QUERY: ', queryset)
     template_name = "index.html"
     paginate_by = 6
