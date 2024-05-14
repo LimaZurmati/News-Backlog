7502db94ec0daf00da3032cb8ab56ed23b3bf751
LimaZurmati <urzalalima@gmail.com>
2024-05-14 06:55:24 +0000

    Add NewsDetail view

--- News/views.py
+++ News/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import News
 
 # Create your views here.
@@ -10,3 +10,26 @@ class NewsList(generic.ListView):
     print('QUERY: ', queryset)
     template_name = "index.html"
     paginate_by = 6
+
+
+class PostDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = News.objects.filter(released_status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by("-created_on")
+        liked = False
+        if post.likes.filter(id=self.request.user_name.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "liked": liked
+            },
+        )
+
+        
