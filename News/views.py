53dde570f08c89a1efad1a3c948e7da001c38e11
LimaZurmati <urzalalima@gmail.com>
2024-05-15 11:02:57 +0000

    Add post method to NewsDetails class in views.py file

--- News/views.py
+++ News/views.py
@@ -34,4 +34,23 @@ class NewsDetail(View):
             },
         )
 
+    def post(self, request, slug, *args, **kwargs):
+        queryset = News.objects.filter(released_status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by("-created_on")
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "news_details.html",
+            {
+                "post": post,
+                "comments": comments,
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
+        )        
+
         
