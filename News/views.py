30cf3fbeb250458328efecb6d167a2ffda0808f1
LimaZurmati <urzalalima@gmail.com>
2024-05-15 10:53:03 +0000

    Create form with method POST also Add crispy

--- News/views.py
+++ News/views.py
@@ -29,7 +29,8 @@ class NewsDetail(View):
             {
                 "post": post,
                 "comments": comments,
-                "liked": liked
+                "liked": liked,
+                "comment_form": CommentForm()
             },
         )
 
