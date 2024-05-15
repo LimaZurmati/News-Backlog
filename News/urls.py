38e10c5a447e9b02410dc5cb7e9aede659493c8d
LimaZurmati <urzalalima@gmail.com>
2024-05-15 20:01:14 +0000

    Add new view comment_edit

--- News/urls.py
+++ News/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 urlpatterns = [
     path("", views.NewsList.as_view(), name="home"),
     path('<slug:slug>/', views.NewsDetail.as_view(), name='news_detail'),
-    path('like/<slug:slug>', views.NewstLike.as_view(), name='news_like'),
+    path('like/<slug:slug>', views.NewsLike.as_view(), name='news_like'),
 ]
--- News/views.py
+++ News/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
 from django.http import HttpResponseRedirect
-from .models import News
+from .models import News, Comment
 from .forms import CommentForm
 
 # Create your views here.
@@ -67,11 +67,32 @@ class NewsDetail(View):
 class NewsLike(View):
     
     def post(self, request, slug, *args, **kwargs):
-        post = get_object_or_404(Post, slug=slug)
+        post = get_object_or_404(News, slug=slug)
         if post.likes.filter(id=request.user.id).exists():
             post.likes.remove(request.user)
         else:
             post.likes.add(request.user)
 
         return HttpResponseRedirect(reverse('news_detail', args=[slug]))
-        
+
+def comment_edit(request, slug, comment_id):
+    """
+    view to edit comments
+    """
+    if request.method == "POST":
+
+        queryset = Post.objects.filter(released_status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comment = get_object_or_404(Comment, pk=comment_id)
+        comment_form = CommentForm(data=request.POST, instance=comment)
+
+        if comment_form.is_valid() and comment.news_author == request.news_user:
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.approved = False
+            comment.save()
+            messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
+        else:
+            messages.add_message(request, messages.ERROR, 'Error updating comment!')
+
+    return HttpResponseRedirect(reverse('news_detail', args=[slug]))
