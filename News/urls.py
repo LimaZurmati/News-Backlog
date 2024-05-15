e9476ec046a0b0f279631e00a7c1fd6a0539f888
LimaZurmati <urzalalima@gmail.com>
2024-05-15 20:04:48 +0000

    Create URL for new view comment_edit

--- News/urls.py
+++ News/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 urlpatterns = [
     path("", views.NewsList.as_view(), name="home"),
     path('<slug:slug>/', views.NewsDetail.as_view(), name='news_detail'),
+    path('<slug:slug>/edit_comment/<int:comment_id>',
+         views.comment_edit, name='comment_edit'),
     path('like/<slug:slug>', views.NewsLike.as_view(), name='news_like'),
 ]
